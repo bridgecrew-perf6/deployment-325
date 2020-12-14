a27cc6c63093f075b0ec5e968565c6dd6d95e7c8
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2020-12-14 13:18:26 +0200

    Fix domain

--- lets-encrypt.sh
+++ lets-encrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(xpeditepermits.com demo.xpeditepermits.com)
+domains=(veles.services)
 rsa_key_size=4096
 data_path="./data/prod/certbot"
 email="evgheni.calcutin@gmail.com" # Adding a valid address is strongly recommended
